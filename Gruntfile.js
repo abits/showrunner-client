6668dd263bb01e0769706b10a84405cd2c786f56
Chris Martel <chris@codeways.org>
2014-09-10 21:48:15 +0200

    something different

--- Gruntfile.js
+++ Gruntfile.js
@@ -377,8 +377,8 @@ module.exports = function (grunt) {
       ],
       dist: [
         'compass:dist',
-        'imagemin',
-        'svgmin'
+        //'imagemin',
+        //'svgmin'
       ]
     },
 
