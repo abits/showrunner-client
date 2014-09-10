4e065210fe68981ac61cf6f6bc76cdaf43aa5774
Chris Martel <chris@codeways.org>
2014-09-10 21:34:04 +0200

    something different

--- Gruntfile.js
+++ Gruntfile.js
@@ -423,7 +423,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    'wiredep',
+    //'wiredep',
     'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
