d2b5624b6689d88ac219499c7d3b8d0bdeeac3a5
Chris Martel <chris@codeways.org>
2014-10-01 22:30:00 +0200

    wip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,30 @@
  *
  * Main module of the application.
  */
-angular
-  .module('ShowRunnerApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ShowRunnerApp.services',
-    'ngPrettyJson'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var showrunnerApp = angular.module('showrunnerApp',
+                                   ['ngRoute', 'ngResource', 'ui.bootstrap']);
+
+showrunnerApp.
+   config(['$routeProvider', '$locationProvider',
+     function($routeProvider, $locationProvider) {
+       $routeProvider
+         .when('/download/:showId', {
+           templateUrl: 'download.html',
+           controller: 'DownloadCtrl',
+           controllerAs: 'download'
+         })
+         .when('/info/:showId', {
+           templateUrl: 'info.html',
+           controller: 'InfoCtrl',
+           controllerAs: 'info'
+         })
+         .when('/', {
+           templateUrl: 'main.html',
+           controller: 'ShowListCtrl',
+           controllerAs: 'list'
+         })
+         ;
+
+       // configure html5 to get links working on jsfiddle
+       $locationProvider.html5Mode(true);
+   }])
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,33 +7,6 @@
  * # MainCtrl
  * Controller of the ShowRunnerApp
  */
- var showrunnerApp = angular.module('showrunnerApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
-
-showrunnerApp.
-  config(['$routeProvider', '$locationProvider',
-    function($routeProvider, $locationProvider) {
-      $routeProvider
-        .when('/download/:showId', {
-          templateUrl: 'download.html',
-          controller: 'DownloadCtrl',
-          controllerAs: 'download'
-        })
-        .when('/info/:showId', {
-          templateUrl: 'info.html',
-          controller: 'InfoCtrl',
-          controllerAs: 'info'
-        })
-        .when('/', {
-          templateUrl: 'main.html',
-          controller: 'ShowListCtrl',
-          controllerAs: 'list'
-        })
-        ;
-
-      // configure html5 to get links working on jsfiddle
-      $locationProvider.html5Mode(true);
-  }])
-
  showrunnerApp.controller('ShowListCtrl', ['$scope', 'CurrentShowList',
     function ($scope, CurrentShowList) {
 
