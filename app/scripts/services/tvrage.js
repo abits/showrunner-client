7c9f1a62f32fe711077bbb464d981762563faa33
Chris Martel <chris@codeways.org>
2014-10-02 14:42:24 +0200

    fresh start

--- app/scripts/services/tvrage.js
+++
@@ -1,5 +0,0 @@
-angular.module('ShowRunnerApp.services', ['ngResource'])
-  .factory('fullShowInfo', function($resource){
-    return $resource('http://services.tvrage.com/feeds/full_show_info.php?sid=2930', {})
-  })
-  .value('version', '0.1');
